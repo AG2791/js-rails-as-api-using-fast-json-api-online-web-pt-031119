ab5a71cb2afa2ba70127aba4b46d8f51acc29cae
AJ Guiles <ashiguiles@gmail.com>
2019-10-30 03:05:17 +0000

    Automatically backed up by Learn

--- app/controllers/sightings_controller.rb
+++ app/controllers/sightings_controller.rb
@@ -1,6 +1,12 @@
 class SightingsController < ApplicationController
+  def index
+    sightings = Sighting.all
+    render json: SightingSerializer.new(sightings)
+  end
+  
   def show
     sighting = Sighting.find(params[:id])
     render json: SightingSerializer.new(sighting)
+     ##created by json_fastAPI
   end
 end
